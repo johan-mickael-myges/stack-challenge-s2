927f0732474a895f7b15cd9d920f13d3dc99a4c1
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-03-08 15:15:04 +0100

    Add pinia

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useCounterStore} from "../stores/counter.ts";
+
+const counterStore = useCounterStore();
+
+function increment() {
+  counterStore.increment();
+}
+
+function resetCounter() {
+  counterStore.$reset();
+}
+
+function add50() {
+  counterStore.$patch({
+    count: counterStore.count + 50
+  });
+}
+
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <button @click="increment">Increment</button>
+  <button @click="add50">+50</button>
+  <button @click="resetCounter">Reset</button>
+  {{ counterStore.count }}
+</template>
