68cbf950edaaeee167e6544ec8d479044edbf108
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-03-08 18:51:18 +0100

    Add mdi

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,20 @@ import './index.css';
 
 import App from './App.vue';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
 const app = createApp(App);
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 app.use(createPinia());
+app.use(vuetify);
 app.mount('#app');
