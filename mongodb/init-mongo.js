5396d06269682b344de30fca91e0cc2272690216
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-06-15 15:14:04 +0200

    Revert "Revert "Remove unused mongodb dir""
    This reverts commit cb50d71278a97e86aa48a030d5a92dccc26970a9.

--- mongodb/init-mongo.js
+++
@@ -1,9 +0,0 @@
-// init-mongo.js
-db = db.getSiblingDB('moonshine');
-
-// Create a user
-db.createUser({
-    user: 'group3',
-    pwd: '123456789',
-    roles: [{ role: 'readWrite', db: 'moonshine' }]
-});
