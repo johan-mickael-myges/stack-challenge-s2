16d894e6c5245ef4dd5a13e080afdf58487442c7
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-06-23 00:40:15 +0200

    Remove admin registration

--- frontend/src/routes/public/adminSecurityRoutes.ts
+++ frontend/src/routes/public/adminSecurityRoutes.ts
@@ -1,16 +1,11 @@
 import { RouteRecordRaw } from 'vue-router';
 import Login from "@/views/public/admin/Login.vue";
-import Register from '@/views/public/admin/Register.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/login/admin',
         component: Login,
     },
-    {
-        path: '/register/admin',
-        component: Register,
-    }
 ];
 
 export default routes;
