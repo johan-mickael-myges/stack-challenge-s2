6f31963435cf640566edbf02781bd6f3a27de5a5
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-07-17 22:54:54 +0200

    Fix product images upload

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,7 +1,7 @@
 const { Product } = require('~models');
 const {buildQueryOptions} = require("~utils/queryOptionsFactory");
 const repository = require("~repositories/productRepository");
-const { uploadToS3 } = require('~services/s3Service');
+const { uploadToS3, generateFileDestination } = require('~services/s3Service');
 
 exports.countProducts = async (req, res, next) => {
     try {
@@ -37,19 +37,25 @@ exports.getProductById = async (req, res, next) => {
 
 exports.createProduct = async (req, res, next) => {
     try {
+        const thumbnailFile = req.files['thumbnail'][0];
+        const imagesFiles = req.files['images'];
+
+        const thumbnailDestination = await generateFileDestination(thumbnailFile, 'products');
+        const imagesDestinations = await Promise.all(imagesFiles.map(async (image) => {
+            return await generateFileDestination(image, 'products');
+        }));
+
         if (req.files) {
-            req.body.thumbnail = await uploadToS3(req.files['thumbnail'][0], 'products', false);
-            req.body.images = await Promise.all(req.files['images'].map(async (image) => {
-                return await uploadToS3(image, 'products', false);
-            }));
+            req.body.thumbnail = thumbnailDestination.url;
+            req.body.images = imagesDestinations.map(image => image.url);
         }
 
         const product = await Product.create(req.body);
 
         if (req.files) {
-            await uploadToS3(req.files['thumbnail'][0], 'products');
-            await Promise.all(req.files['images'].map(async (image) => {
-                return await uploadToS3(image, 'products');
+            await uploadToS3(thumbnailFile, thumbnailDestination);
+            await Promise.all(imagesFiles.map(async (image, index) => {
+                await uploadToS3(image, imagesDestinations[index]);
             }));
         }
 
--- backend/src/services/s3Service.js
+++ backend/src/services/s3Service.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const slug = require('slug');
 const path = require('path');
 
-const uploadToS3 = async (file, folder = 'products', upload = true) => {
+const generateFileDestination = async (file, folder = 'uploads') => {
     if (!file) {
         throw new Error('No file provided');
     }
@@ -12,21 +12,36 @@ const uploadToS3 = async (file, folder = 'products', upload = true) => {
     const extension = path.extname(file.originalname);
     const sanitizedFileName = slug(`${uuidv4()}-${path.basename(file.originalname, extension)}`, { lower: true }) + extension;
 
+    const destination = `${folder}/${sanitizedFileName}`;
+
+    return {
+        destination: destination,
+        url: `https://${bucket}.s3.amazonaws.com/${destination}`
+    };
+}
+
+const uploadToS3 = async (file, destination = {}) => {
+    if (!file) {
+        throw new Error('No file provided');
+    }
+
+    if (!destination) {
+        throw new Error('No destination provided');
+    }
     const uploadParams = {
         Bucket: bucket,
-        Key: `${folder}/${sanitizedFileName}`,
+        Key: destination.destination,
         Body: file.buffer,
         ContentType: file.mimetype,
     };
 
-    if (upload) {
-        const command = new PutObjectCommand(uploadParams);
-        await s3Client.send(command);
-    }
+    const command = new PutObjectCommand(uploadParams);
+    await s3Client.send(command);
 
-    return `https://${bucket}.s3.amazonaws.com/${uploadParams.Key}`;
+    return destination.url;
 };
 
 module.exports = {
     uploadToS3,
+    generateFileDestination,
 };
