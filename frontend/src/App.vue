c41b5c63514c39f96556b4368542e0582ded79fc
racha <rachaafaf15@gmail.com>
2024-07-17 22:54:36 +0200

    cookies front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import CookieConsent from './components/cookies/CookieConsent.vue';
 </script>
 
 <template>
   <v-app>
     <router-view></router-view>
+    <CookieConsent />
   </v-app>
 </template>
