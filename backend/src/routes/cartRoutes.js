42bec5450c8991692ba70d69ed59430068211d88
racha <rachaafaf15@gmail.com>
2024-07-18 12:19:02 +0200

    change quantity

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -3,9 +3,9 @@ const cartController = require('~controllers/cartController');
 const router = express.Router();
 const { checkToken, authorizeRoles } = require('~middlewares/authMiddleware');
 
-
 router.post('/', checkToken, cartController.addToCart);
 router.get('/', checkToken, cartController.getCartItems);
 router.delete('/:productId', checkToken, cartController.removeFromCart);
+router.patch('/:productId', checkToken, cartController.updateCartItemQuantity);
 
 module.exports = router;
