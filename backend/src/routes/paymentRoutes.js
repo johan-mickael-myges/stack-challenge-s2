011e0a09634fca048fc8b282d51026ce8250199c
racha <rachaafaf15@gmail.com>
2024-07-23 23:13:14 +0200

    securité routes

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('~controllers/paymentController');
+const { checkToken } = require('~middlewares/authMiddleware');
 
-router.post('/create', paymentController.createOrder);
-router.post('/capture', paymentController.captureOrder);
+router.post('/create', checkToken, paymentController.createOrder);
+router.post('/capture', checkToken, paymentController.captureOrder);
 
 module.exports = router;
