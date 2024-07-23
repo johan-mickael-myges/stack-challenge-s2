3c0b12f8b9d111f0cb27ed37b5f210f531643c6f
hema-brm <hema932001@gmail.com>
2024-07-23 23:57:58 +0200

    footer

---
+++ frontend/src/components/Footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-footer app class="shadow-lg d-flex justify-center align-center pa-4">
+      <v-container class="d-flex justify-center">
+        <v-row>
+          <v-col class="text-center">
+            <span>© 2024 Layalin | </span>
+            <router-link to="/privacy-policy" class="text-primary">Politique de confidentialité</router-link>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  };
+  </script>
+  
--- frontend/src/layouts/UserLayout.vue
+++ frontend/src/layouts/UserLayout.vue
@@ -1,20 +1,23 @@
 <template>
     <v-app class="font-sans">
       <AppBar/>
-      <v-main>
+      <v-main class="mb-4">
         <router-view class="p-4"></router-view>
       </v-main>
+      <Footer/>
     </v-app>
   </template>
   
   <script lang="ts">
   import { defineComponent } from 'vue';
   import AppBar from '@/components/Bar/AppBar.vue';
+  import Footer from '@/components/Footer/Footer.vue';
   
   export default defineComponent({
     name: 'AdminLayout',
     components: {
       AppBar,
+      Footer,
     },
   });
   </script>
