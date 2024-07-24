cb17a82d15794b6fdae6655b923567e05c39bbe9
racha <rachaafaf15@gmail.com>
2024-07-24 18:00:25 +0200

    billing adress works:)

--- backend/src/models/delivery.js
+++ backend/src/models/delivery.js
@@ -14,98 +14,146 @@ class Delivery extends Model {
 
 module.exports = (sequelize) => {
     Delivery.init(
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    autoIncrement: true,
-                    primaryKey: true,
-                },
-                firstName: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: {
-                        notEmpty: {
-                            msg: 'Le prénom du destinataire est requis',
-                        },
-                        len: {
-                            args: [1, 255],
-                            msg: 'Le prénom du destinataire doit comporter entre 1 et 255 caractères',
-                        },
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Le prénom du destinataire est requis',
                     },
-                },
-                lastName: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: {
-                        notEmpty: {
-                            msg: 'Le nom du destinataire est requis',
-                        },
-                        len: {
-                            args: [1, 255],
-                            msg: 'Le nom du destinataire doit comporter entre 1 et 255 caractères',
-                        },
+                    len: {
+                        args: [1, 255],
+                        msg: 'Le prénom du destinataire doit comporter entre 1 et 255 caractères',
                     },
-                },
-                address: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: {
-                        notEmpty: {
-                            msg: 'L\'adresse est requise',
-                        },
-                    },
-                },
-                phoneNumber: {
-                    type: DataTypes.STRING,
-                    allowNull: true,
-                    validate: {
-                        len: {
-                            args: [10, 15],
-                            msg: 'Le numéro de téléphone doit comporter entre 10 et 15 caractères',
-                        },
-                        isNumeric: {
-                            msg: 'Le numéro de téléphone doit contenir uniquement des chiffres',
-                        },
-                    },
-                },
-                status: {
-                    type: DataTypes.ENUM('PENDING', 'SHIPPED', 'DELIVERED', 'CANCELLED'),
-                    allowNull: false,
-                    defaultValue: 'PENDING',
-                    validate: {
-                        isIn: {
-                            args: [['PENDING', 'SHIPPED', 'DELIVERED', 'CANCELLED']],
-                            msg: 'Le statut doit être PENDING, SHIPPED, DELIVERED ou CANCELLED',
-                        },
-                    },
-                },
-                shippingMethodId: {
-                    type: DataTypes.INTEGER,
-                    references: {
-                        model: 'shipping_methods',
-                        key: 'id'
-                    },
-                    allowNull: true, // Allow null values
-                    onDelete: 'SET NULL',
-                    onUpdate: 'CASCADE'
-                },
-                orderId: {
-                    type: DataTypes.INTEGER,
-                    references: {
-                        model: 'orders',
-                        key: 'id'
-                    },
-                    allowNull: false,
-                    onDelete: 'CASCADE',
-                    onUpdate: 'CASCADE'
                 },
             },
-            {
-                sequelize,
-                modelName: 'Delivery',
-                tableName: 'deliveries',
-                timestamps: true,
-            }
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Le nom du destinataire est requis',
+                    },
+                    len: {
+                        args: [1, 255],
+                        msg: 'Le nom du destinataire doit comporter entre 1 et 255 caractères',
+                    },
+                },
+            },
+            address: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'L\'adresse est requise',
+                    },
+                },
+            },
+            phoneNumber: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                validate: {
+                    len: {
+                        args: [10, 15],
+                        msg: 'Le numéro de téléphone doit comporter entre 10 et 15 caractères',
+                    },
+                    isNumeric: {
+                        msg: 'Le numéro de téléphone doit contenir uniquement des chiffres',
+                    },
+                },
+            },
+            status: {
+                type: DataTypes.ENUM('PENDING', 'SHIPPED', 'DELIVERED', 'CANCELLED'),
+                allowNull: false,
+                defaultValue: 'PENDING',
+                validate: {
+                    isIn: {
+                        args: [['PENDING', 'SHIPPED', 'DELIVERED', 'CANCELLED']],
+                        msg: 'Le statut doit être PENDING, SHIPPED, DELIVERED ou CANCELLED',
+                    },
+                },
+            },
+            shippingMethodId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'shipping_methods',
+                    key: 'id'
+                },
+                allowNull: true, // Allow null values
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE'
+            },
+            orderId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'orders',
+                    key: 'id'
+                },
+                allowNull: false,
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            billingFirstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Le prénom de facturation est requis',
+                    },
+                    len: {
+                        args: [1, 255],
+                        msg: 'Le prénom de facturation doit comporter entre 1 et 255 caractères',
+                    },
+                },
+            },
+            billingLastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'Le nom de facturation est requis',
+                    },
+                    len: {
+                        args: [1, 255],
+                        msg: 'Le nom de facturation doit comporter entre 1 et 255 caractères',
+                    },
+                },
+            },
+            billingAddress: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'L\'adresse de facturation est requise',
+                    },
+                },
+            },
+            billingPhoneNumber: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                validate: {
+                    len: {
+                        args: [10, 15],
+                        msg: 'Le numéro de téléphone de facturation doit comporter entre 10 et 15 caractères',
+                    },
+                    isNumeric: {
+                        msg: 'Le numéro de téléphone de facturation doit contenir uniquement des chiffres',
+                    },
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Delivery',
+            tableName: 'deliveries',
+            timestamps: true,
+        }
     );
 
     return Delivery;
